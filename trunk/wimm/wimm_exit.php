949cb3c70659ba659c8a8dc9f9fc8868782a410a
ifdru74@gmail.com <ifdru74@gmail.com@2e5c0dbf-2484-ce92-10b9-89b7b79ff6a9>
2014-09-03 13:20:42 +0000

    refactoring to PDO

--- trunk/wimm/wimm_exit.php
+++ trunk/wimm/wimm_exit.php
@@ -1,24 +1,27 @@
 <?php
-	if (!ini_get('register_globals')) {
-	   $superglobals = array($_SERVER, $_ENV,
-	       $_FILES, $_COOKIE, $_POST, $_GET);
-	   if (isset($_SESSION)) {
-	       array_unshift($superglobals, $_SESSION);
-	   }
-	   foreach ($superglobals as $superglobal) {
-	       extract($superglobal, EXTR_SKIP);
-	   }
-	}
-	$dest_page = "wimm_auth.php";
-	session_start();
-	include("fun_web.php");
-	$uid = getSessionParam("UID",0);
-	if($uid!=0)	{		session_destroy();  	}
-	$url = "http://" . $_SERVER['HTTP_HOST']
-		. dirname($_SERVER['PHP_SELF'])
-		. "/" . $dest_page;
-	header("Location: $url");
-	print_head("Завершение сеанса");
+    if (!ini_get('register_globals')) {
+       $superglobals = array($_SERVER, $_ENV,
+           $_FILES, $_COOKIE, $_POST, $_GET);
+       if (isset($_SESSION)) {
+           array_unshift($superglobals, $_SESSION);
+       }
+       foreach ($superglobals as $superglobal) {
+           extract($superglobal, EXTR_SKIP);
+       }
+    }
+    $dest_page = "wimm_auth.php";
+    session_start();
+    include("fun_web.php");
+    $uid = getSessionParam("UID",0);
+    if($uid!=0)	{
+        update_param("UID",0);
+        session_destroy();
+    }
+    $url = "http://" . $_SERVER['HTTP_HOST']
+            . dirname($_SERVER['PHP_SELF'])
+            . "/" . $dest_page;
+    header("Location: $url");
+    print_head("Завершение сеанса");
 ?>
 <script language="JavaScript" type="text/JavaScript">
 function bodyOnLoad()
