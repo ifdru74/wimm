43350f410a233e7aa20d98659bdbdef103b68c26
ifdru74@gmail.com <ifdru74@gmail.com@2e5c0dbf-2484-ce92-10b9-89b7b79ff6a9>
2014-09-03 12:55:01 +0000

    refactoring to PDO

--- wimm/wimm_auth.php
+++ wimm/wimm_auth.php
@@ -15,37 +15,69 @@
 	include("fun_mysql.php");
 	$user_name = getRequestParam("UNAME","");
 	$cnt = "-1";
+//	if(strlen($user_name)>0)	{
+//		$conn = f_get_connection();
+//	    if($conn)	{
+//	    	$uname=mysql_real_escape_string($user_name,$conn);
+//			$upasswd=mysql_real_escape_string(getRequestParam("UPASS",""),$conn);
+//	    	$sql = "select user_id as cnt, user_name from m_users where user_login='$uname' and user_password=PASSWORD('$upasswd')";
+//	    	$res = mysql_query($sql,$conn);
+//	    	if($res)	{
+//	    		$row = mysql_fetch_assoc($res);
+//	    		if($row)	{
+//	    			$cnt = $row['cnt'];
+//	    			$user_name = $row['user_name'];
+//	    			if($cnt!=0)	{
+//					$uid = $cnt;
+//	    				$_SESSION["UID"] = $cnt;
+//	    				$_SESSION["UNAME"] = $user_name;
+//	    				$url = "http://" . $_SERVER['HTTP_HOST']
+//	    					. dirname($_SERVER['PHP_SELF'])
+//	    					. "/index.php";
+//	    				header("Location: $url");
+//	    			}	    		}
+//	    		else
+//	    			$cnt = "!row";	    	}
+//	    	else
+//	    		$cnt = "!res";
+//		}
+//		else
+//			$cnt = "!conn";
+//	}
+//	else
+//		$cnt = strlen($user_name) . " - " .$user_name;
 	if(strlen($user_name)>0)	{
-		$conn = f_get_connection();
+            $conn = f_get_connection();
 	    if($conn)	{
-	    	$uname=mysql_real_escape_string($user_name,$conn);
-			$upasswd=mysql_real_escape_string(getRequestParam("UPASS",""),$conn);
+	    	$uname=value4db($user_name);
+			$upasswd=value4db(getRequestParam("UPASS",""));
 	    	$sql = "select user_id as cnt, user_name from m_users where user_login='$uname' and user_password=PASSWORD('$upasswd')";
-	    	$res = mysql_query($sql,$conn);
+	    	$res = $conn->query($sql);
 	    	if($res)	{
-	    		$row = mysql_fetch_assoc($res);
-	    		if($row)	{
-	    			$cnt = $row['cnt'];
-	    			$user_name = $row['user_name'];
-	    			if($cnt!=0)	{
-					$uid = $cnt;
-	    				$_SESSION["UID"] = $cnt;
-	    				$_SESSION["UNAME"] = $user_name;
-	    				$url = "http://" . $_SERVER['HTTP_HOST']
-	    					. dirname($_SERVER['PHP_SELF'])
-	    					. "/index.php";
-	    				header("Location: $url");
-	    			}	    		}
-	    		else
-	    			$cnt = "!row";	    	}
+                    $row = $res->fetch(PDO::FETCH_ASSOC);
+                    if($row)	{
+                        $cnt = $row['cnt'];
+                        $user_name = $row['user_name'];
+                        if($cnt!=0)	{
+                            $uid = $cnt;
+                            $_SESSION["UID"] = $cnt;
+                            $_SESSION["UNAME"] = $user_name;
+                            $url = "http://" . $_SERVER['HTTP_HOST']
+                                    . dirname($_SERVER['PHP_SELF'])
+                                    . "/index.php";
+                            header("Location: $url");
+                        }	    		}
+                    else
+                        $cnt = "!row";
+                }
 	    	else
-	    		$cnt = "!res";
-		}
-		else
-			$cnt = "!conn";
+                    $cnt = "!res";
+            }
+            else
+                $cnt = "!conn";
 	}
 	else
-		$cnt = strlen($user_name) . " - " .$user_name;
+            $cnt = strlen($user_name) . " - " .$user_name;        
 	print_head("Авторизация");
 	if($uid!=0)	{
 ?>
@@ -74,10 +106,11 @@
         <DIV style="margin: 0px auto;">
 <?php
 		if(strcmp($cnt,"1")!=0)	{
-			print "<TR class=\"hidden\"><TD COLSPAN=\"2\" class=\"hidden\">";
-			if($cnt!=0)
-				showError("Неверное имя пользователя или пароль ($cnt)");
-			print "</TD></TR>\n";		}
+                    print "<TR class=\"hidden\"><TD COLSPAN=\"2\" class=\"hidden\">";
+                    if($cnt!=0)
+                        showError("Неверное имя пользователя или пароль ($cnt)");
+                    print "</TD></TR>\n";
+                }
 ?>
             <DIV style="height: 30px; width:300px;margin: 0px auto;">
                 <label for="UNAME">Имя пользователя:</LABEL>
@@ -87,7 +120,7 @@
                 <label for="UPASS">Пароль:</LABEL>
                 <input style="float: right;" id="UPASS" name="UPASS" type="password" value="<?php echo $passwd;?>">
             </DIV>
-            <DIV style="height: 25px; width:300px;margin: 0px auto;">
+            <DIV style="height: 30px; width:300px;margin: 0px auto;">
                 <input style="width:100px;float: left;" type="submit" value="Вход">
                 <input style="width:100px;float: right;" type="reset" value="Очистить">
             </DIV>
