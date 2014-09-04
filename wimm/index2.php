e6b4b8e3f312d674a73f8693fbb0f722aa8e8a30
ifdru74@gmail.com <ifdru74@gmail.com@2e5c0dbf-2484-ce92-10b9-89b7b79ff6a9>
2014-09-04 19:08:56 +0000

    bugfix

--- wimm/index2.php
+++ wimm/index2.php
@@ -17,8 +17,9 @@
         <link rel="SHORTCUT ICON" href="picts/favicon.ico">
         <title>Семейный бюджет</title>
     </head>
-<body>
+    <body onload="$('#dialog_box').draggable();$('#OK_BTN').draggable();">
     <script language="JavaScript" type="text/JavaScript" src="js/form_common.js"></script>
+    <script language="JavaScript" type="text/JavaScript" src="js/jquery-ui.js"></script>
 <?php    
     if(isMSIE())   {
 ?>        
@@ -47,7 +48,6 @@
             if(row_id.length<1) {
                 $("#HIDDEN_ID").val(0);
                 $("#OK_BTN").val("Добавить");
-                $("#FRM_MODE").val("insert");
                 $("#dlg_box_cap").text("Добавление записи");
                 s1 = "";
                 $("#t_sum").val(s1);
@@ -59,10 +59,13 @@
             else    {
                 $("#HIDDEN_ID").val(row_id);
                 $("#OK_BTN").val("Изменить");
-                $("#FRM_MODE").val("update");
                 $("#dlg_box_cap").text("Изменение записи");
-                $("#t_name").val($("TNAME[" + row_id + "]").text());
-                $("#t_sum").val($("T_SUMM[" + row_id + "]").text());
+                $("#t_name").val($("#TNAME_" + row_id).text());
+                $("#t_sum").val($("#T_SUMM_" + row_id).attr('title'));
+                $("#t_user").val($("#T_USR_" + row_id).val());
+                $("#t_place").val($("#T_PLACE_" + row_id).val());
+                $("#t_budget").val($("#T_BUDG_" + row_id).val());
+                $("#t_date").val($("#T_DATE_" + row_id).attr('title'));
             }
         }
     </script>
@@ -80,10 +83,10 @@ function print_buttons($conn, $bd="",$ed="", $bg="-1")
             <label for="EDATE">Дата окончания периода:</label>
             <input id="EDATE" name="EDATE" type="text" value="<?php echo $ed;?>">
             <label for="f_budget">Бюджет:</label>
-            <select size="1" id="f_budget" name="f_budget">
+            <select size="1" id="f_budget" name="f_budget" onchange="$('#expenses').submit();">
 <?php
             $sql = "SELECT budget_id, budget_name FROM m_budget WHERE close_date is null";
-            f_set_sel_options2($conn, $sql, $bg, 1, 2);
+            f_set_sel_options2($conn, $sql, $bg, $bg, 2);
 ?>
             </select>
         </div>
@@ -102,15 +105,15 @@ function print_buttons($conn, $bd="",$ed="", $bg="-1")
 }
 if($conn)	{
 	$fm = getRequestParam("FRM_MODE","refresh");
-	$sql = "";
+	$sql_dml = "";
 	if(strcmp($fm,"insert")==0)	{
-		$sql = "INSERT INTO m_transactions (transaction_name, t_type_id, currency_id, transaction_sum, transaction_date, user_id, open_date, place_id, budget_id) VALUES(";
+		$sql_dml = "INSERT INTO m_transactions (transaction_name, t_type_id, currency_id, transaction_sum, transaction_date, user_id, open_date, place_id, budget_id) VALUES(";
 		$s = value4db(urldecode(getRequestParam("t_name","Покупка!")));
-		$sql .= "'$s',";
+		$sql_dml .= "'$s',";
 		$s = getRequestParam("t_type",1);
-		$sql .= "$s,";
+		$sql_dml .= "$s,";
 		$s = getRequestParam("t_curr",2);
-		$sql .= "$s,";
+		$sql_dml .= "$s,";
 		$s1 = getRequestParam("t_sum",0);
 		if(strpos($s1,",")===false)	{
 			$s = $s1;
@@ -118,16 +121,16 @@ if($conn)	{
 		else	{
 			$s = str_replace(",",".",$s1);
 		}
-		$sql .= "$s,";
+		$sql_dml .= "$s,";
 		$td = getRequestParam("t_date",date("Y-m-d H:i:s"));
-		$sql .= "'$td',";
+		$sql_dml .= "'$td',";
 		$s = getRequestParam("t_user",1);
-		$sql .= "$s,";
-		$sql .= "'$td',";
+		$sql_dml .= "$s,";
+		$sql_dml .= "'$td',";
 		$s = getRequestParam("t_place",0);
-		$sql .= "$s,";
+		$sql_dml .= "$s,";
 		$s = getRequestParam("t_budget",0);
-		$sql .= "$s)";
+		$sql_dml .= "$s)";
 	}
         $cd = getdate();
 	$m = $cd['mon'];
@@ -146,17 +149,18 @@ if($conn)	{
 		$m = "0" . $m;
 	$ed = update_param("EDATE", "END_DATE", "$y-$m-01");
 	print_body_title("Расходы с $bd по $ed");
-	if(strlen($sql)>0)	{
-		print "	<input ID=\"SQL\" type=\"hidden\" value=\"$sql\">\n";
-		$conn->query($sql);
+	if(strlen($sql_dml)>0)	{
+		print "	<input ID=\"SQL\" type=\"hidden\" value=\"$sql_dml\">\n";
+		$conn->query($sql_dml);
 		$conn->commit();
 	}
+        $s = "";
 ?>
 	<form id="expenses" name="expenses" method="post">
             <input id="FRM_MODE" name="FRM_MODE" type="hidden" value="refresh">
             <input id="HIDDEN_ID" name="HIDDEN_ID" type="hidden" value="0">
             <input name="UID" type="hidden" value="<?php echo getRequestParam("UID", "");?>">
-            <DIV class="dlg_box" id="dialog_box" style="width:600px;display:none;">
+            <DIV class="dlg_box" id="dialog_box" style="width:600px;display:none;" class='ui-widget-content'>
                 <DIV class="dlg_box_cap" id="dlg_box_cap">Изменение записи</DIV>
                 <DIV class="dlg_box_text" id="dlg_box_text" >
                     <TABLE WIDTH="100%" class="hidden">
@@ -226,8 +230,9 @@ if($conn)	{
             </DIV>
 <?php
 	$bg = getRequestParam("f_budget","-1");
-	print_buttons($conn, $bd,$ed,$bg);
+	print_buttons($conn, $bd, $ed, $bg);
 ?>
+            <input type="hidden" id="bg" value="<?php echo $bg;?>">
             <TABLE WIDTH="100%" BORDER="1">
                 <thead>
                     <TR>
@@ -243,7 +248,7 @@ if($conn)	{
 <?php
 	//print "<TR><TD COLSPAN=\"6\">Подключён</TD></TR>\n";
 	$sql = "select transaction_id, t_type_name, transaction_name, transaction_sum, Type_sign, transaction_date, user_name, place_name, " .
-                " place_descr, t.currency_id t_cid, mcu.currency_sign, mb.currency_id as bc_id " .
+                " place_descr, t.currency_id t_cid, mcu.currency_sign, mb.currency_id as bc_id, t.place_id, t.budget_id, t.user_id " .
                 " from m_transactions t, m_transaction_types tt, m_users tu, m_places tp, m_currency mcu, m_budget mb " .
                 " where t.t_type_id=tt.t_type_id and t.user_id=tu.user_id and t.place_id=tp.place_id and t.currency_id=mcu.currency_id and " .
                 " t.budget_id=mb.budget_id and transaction_date>='$bd' and  transaction_date<'$ed' ";
@@ -256,7 +261,6 @@ if($conn)	{
 	//$res = mysql_query($sql,$conn);
 	$sm = 0;
 	$sd = 0;
-	$c_class = "dark";
 	$plus_pict = "picts/plus.gif";
 	$minus_pict = "picts/minus.gif";
 	$locale_info = localeconv();
@@ -264,11 +268,12 @@ if($conn)	{
             while ($row =  $res->fetch(PDO::FETCH_ASSOC)) {
                 print "<TR class=\"expenses\">\n";
                 $row_pk = $row['transaction_id'];
-                print "<TD><input name=\"ROW_ID\" ID=\"ROW_$row_pk\" type=\"radio\" value=\"$row_pk\" onclick=\"sel_row('$row_pk');\">";
+                $row_id = "ROW_" . $row_pk;
+                print "<TD><input name=\"ROW_ID\" ID=\"$row_id\" type=\"radio\" value=\"$row_pk\" onclick=\"sel_row('$row_pk');\">";
                 print "<input class=\"multiselect\" style=\"display:none;\" name=\"MROW[$row_pk]\" ID=\"CHK_$row_pk\" type=\"radio\" value=\"$row_pk\"></TD>\n";
                 $t = $row['t_type_name'];
                 $s = $row['transaction_name'];
-                print "<TD TITLE=\"$t\"><LABEL id=\"TNAME[$row_pk]\" FOR=\"ROW_$row_pk\">$s</LABEL></TD>\n";
+                print "<TD TITLE=\"$t\"><LABEL id=\"TNAME_$row_pk\" FOR=\"$row_id\">$s</LABEL></TD>\n";
                 $cid = $row['t_cid'];
                 $bid = $row['bc_id'];
                 if($cid!=$bid) {
@@ -296,16 +301,26 @@ if($conn)	{
                 if(strlen($pn)>0)	{
                         print "<IMG SRC=$pn>&nbsp;";
                 }
-                $t = number_format($row['transaction_sum'],2,","," ");
-                print "$cs<LABEL ID=\"T_SUMM[$row_pk]\">$t</LABEL></TD>\n";
+                $ts = $row['transaction_sum'];
+                $t = number_format($ts,2,","," ");
+                printf('%s<LABEL ID="T_SUMM_%s" FOR="%s" title="%s">%s</LABEL></TD>%s',
+                        $cs, $row_pk, $row_id, $ts, $t, PHP_EOL);
                 $t = $row['transaction_date'];
                 $s = f_get_disp_date($t);
-                print "<TD TITLE=\"$t\">$s</TD>\n";
+                print "<TD id=\"T_DATE_$row_pk\" TITLE=\"$t\">$s</TD>\n";
                 $s = $row['user_name'];
-                print "<TD>$s</TD>\n";
+                $u = filter_array($row, 'user_id', '');
+                print "<TD><label FOR=\"$row_id\">$s</label>" .
+                        "<input type=\"hidden\" id=\"T_USR_$row_pk\" value=\"$u\">".
+                        "</TD>" . PHP_EOL;
                 $s = $row['place_name'];
                 $t = $row['place_descr'];
-                print "<TD TITLE=\"$t\">$s</TD>\n";
+                $p = filter_array($row, 'place_id', '');
+                $b = filter_array($row, 'budget_id', '');
+                $sCol = "<TD TITLE=\"$t\"><label FOR=\"$row_id\">$s</label>".
+                        "<input type=\"hidden\" id=\"T_PLACE_$row_pk\" value=\"$p\">".
+                        "<input type=\"hidden\" id=\"T_BUDG_$row_pk\" value=\"$b\"></TD>" . PHP_EOL;
+                print $sCol;
                 print "</TR>\n";
             }	
         }
