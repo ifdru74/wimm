c6b461666539e7da925396fd1c41ec1117a100ec
ifdru74@gmail.com <ifdru74@gmail.com@2e5c0dbf-2484-ce92-10b9-89b7b79ff6a9>
2014-09-12 19:47:49 +0000

    bugfix

--- wimm/wimm_report.php
+++ wimm/wimm_report.php
@@ -1,4 +1,6 @@
 <?php
+    $t = time() + 10;
+    header("Expires: " . date("D, d M Y H:i:s T", $t));
 	if (!ini_get('register_globals')) {
 	   $superglobals = array($_SERVER, $_ENV,
 	       $_FILES, $_COOKIE, $_POST, $_GET);
@@ -12,7 +14,7 @@
 	session_start();
 	include("fun_web.php");
 	auth_check('UID');
-	print_head("Отчёт по затратам в нескольких разрезах");
+	print_head("РћС‚С‡С‘С‚ РїРѕ Р·Р°С‚СЂР°С‚Р°Рј РІ РЅРµСЃРєРѕР»СЊРєРёС… СЂР°Р·СЂРµР·Р°С…");
 ?>
 <script language="JavaScript" type="text/JavaScript">
 function doEdit(s1)
@@ -25,19 +27,19 @@ function doEdit(s1)
 <body>
 <?php
 include_once 'fun_dbms.php';
-function print_buttons($bd="",$ed="", $bg="-1")
+function print_buttons($conn, $bd="",$ed="", $bg="-1")
 {	print "<TABLE WIDTH=\"100%\" class=\"hidden\">\n";
 	print "\t<TR class=\"hidden\">\n";
 	if(strlen($bd)>0)	{
-		print "\t\t<TD class=\"hidden\">Дата начала периода:<input name=\"BDATE\" type=\"text\" value=\"$bd\"></TD>\n";
-		print "\t\t<TD class=\"hidden\">Дата окончания периода:<input name=\"EDATE\" type=\"text\" value=\"$ed\"></TD>\n";
-		print "\t\t<TD class=\"hidden\">Бюджет:<select size=\"1\" name=\"f_budget\">\n";
+		print "\t\t<TD class=\"hidden\">Р”Р°С‚Р° РЅР°С‡Р°Р»Р° РїРµСЂРёРѕРґР°:<input name=\"BDATE\" type=\"text\" value=\"$bd\"></TD>\n";
+		print "\t\t<TD class=\"hidden\">Р”Р°С‚Р° РѕРєРѕРЅС‡Р°РЅРёСЏ РїРµСЂРёРѕРґР°:<input name=\"EDATE\" type=\"text\" value=\"$ed\"></TD>\n";
+		print "\t\t<TD class=\"hidden\">Р‘СЋРґР¶РµС‚:<select size=\"1\" name=\"f_budget\">\n";
 		$sql = "SELECT budget_id, budget_name FROM m_budget WHERE close_date is null";
-		f_set_sel_options2($sql, $bg, 1, 2);
+		f_set_sel_options2($conn, $sql, $bg, 1, 2);
 		print "</select></TD>\n";
 	}
-	print "\t\t<TD class=\"hidden\"><input type=\"submit\" value=\"Обновить\"></TD>\n";
-	print "\t\t<TD class=\"hidden\"><input type=\"button\" value=\"Выход\" onclick=\"doEdit('exit')\"></TD>\n";
+	print "\t\t<TD class=\"hidden\"><input type=\"submit\" value=\"РћР±РЅРѕРІРёС‚СЊ\"></TD>\n";
+	print "\t\t<TD class=\"hidden\"><input type=\"button\" value=\"Р’С‹С…РѕРґ\" onclick=\"doEdit('exit')\"></TD>\n";
 	print "\t</TR>\n";
 	print "</TABLE>\n";
 }
@@ -60,24 +62,24 @@ if($conn)	{
 	if(strlen($m)<2)
 		$m = "0" . $m;
 	$ed = update_param("EDATE", "END_DATE", "$y-$m-01");
-	print_body_title("Где потратили деньги с $bd по $ed");
+	print_body_title("Р“РґРµ РїРѕС‚СЂР°С‚РёР»Рё РґРµРЅСЊРіРё СЃ $bd РїРѕ $ed");
 	print "<form name=\"expenses\" action=\"wimm_report.php\" method=\"post\">\n";
-	if(strlen($sql)>0)	{
-		print "	<input name=\"SQL\" type=\"hidden\" value=\"$sql\">\n";
-		mysql_query($sql, $conn);
-		mysql_query("commit",$conn);
-	}
+//	if(strlen($sql)>0)	{
+//		print "	<input name=\"SQL\" type=\"hidden\" value=\"$sql\">\n";
+//		mysql_query($sql, $conn);
+//		mysql_query("commit",$conn);
+//	}
 	print "<input name=\"FRM_MODE\" type=\"hidden\" value=\"refresh\">\n";
 	$bg = getRequestParam("f_budget","-1");
-	print_buttons($bd,$ed,$bg);
+	print_buttons($conn, $bd,$ed,$bg);
 	print "<TABLE WIDTH=\"100%\" BORDER=\"1\">\n";
 	print "<TR>\n";
-	print "<TH>Место</TH>\n";
-	print "<TH>Сумма</TH>\n";
-	print "<TH TITLE=\"относится к последней покупке\">Дата и время</TH>\n";
-	print "<TH>Количество</TH>\n";
+	print "<TH>РњРµСЃС‚Рѕ</TH>\n";
+	print "<TH>РЎСѓРјРјР°</TH>\n";
+	print "<TH TITLE=\"РѕС‚РЅРѕСЃРёС‚СЃСЏ Рє РїРѕСЃР»РµРґРЅРµР№ РїРѕРєСѓРїРєРµ\">Р”Р°С‚Р° Рё РІСЂРµРјСЏ</TH>\n";
+	print "<TH>РљРѕР»РёС‡РµСЃС‚РІРѕ</TH>\n";
 	print "</TR>\n";
-	//print "<TR><TD COLSPAN=\"6\">Подключён</TD></TR>\n";
+	//print "<TR><TD COLSPAN=\"6\">РџРѕРґРєР»СЋС‡С‘РЅ</TD></TR>\n";
 	$sql = "select mtp.place_name, sum(transaction_sum) as s_um, max(mt.transaction_date) as ltd, count(*) as cnt from m_transactions mt, m_transaction_types  mtt, m_places mtp where mt.t_type_id=mtt.t_type_id and mtt.Type_sign<0 and mt.place_id=mtp.place_id and transaction_date>='$bd' and  transaction_date<'$ed' ";
 	if($bg>0)	{
 		$sql .= " and mt.budget_id=$bg ";
@@ -89,7 +91,7 @@ if($conn)	{
 	$c_class = "dark";
 	$plus_pict = "picts/plus.gif";
 	$minus_pict = "picts/minus.gif";
-	if($res)	{		//print "<TR><TD COLSPAN=\"6\">Запрос пошёл</TD></TR>\n";
+	if($res)	{		//print "<TR><TD COLSPAN=\"6\">Р—Р°РїСЂРѕСЃ РїРѕС€С‘Р»</TD></TR>\n";
             while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
                 print "<TR class=\"$c_class\">\n";
                 if(strcmp($c_class,"dark")==0)	{
@@ -114,15 +116,15 @@ if($conn)	{
             print "<TR><TD COLSPAN=\"6\">SQL=\"$sql\"<BR>$message</TD></TR>\n";
 	}
 	print "</TABLE>\n";
-	print_title("На что потратили деньги с $bd по $ed");
+	print_title("РќР° С‡С‚Рѕ РїРѕС‚СЂР°С‚РёР»Рё РґРµРЅСЊРіРё СЃ $bd РїРѕ $ed");
 	print "<TABLE WIDTH=\"100%\" BORDER=\"1\">\n";
 	print "<TR>\n";
-	print "<TH>Статья расходов</TH>\n";
-	print "<TH>Сумма</TH>\n";
-	print "<TH TITLE=\"относится к последней покупке\">Дата и время</TH>\n";
-	print "<TH>Количество</TH>\n";
+	print "<TH>РЎС‚Р°С‚СЊСЏ СЂР°СЃС…РѕРґРѕРІ</TH>\n";
+	print "<TH>РЎСѓРјРјР°</TH>\n";
+	print "<TH TITLE=\"РѕС‚РЅРѕСЃРёС‚СЃСЏ Рє РїРѕСЃР»РµРґРЅРµР№ РїРѕРєСѓРїРєРµ\">Р”Р°С‚Р° Рё РІСЂРµРјСЏ</TH>\n";
+	print "<TH>РљРѕР»РёС‡РµСЃС‚РІРѕ</TH>\n";
 	print "</TR>\n";
-	//print "<TR><TD COLSPAN=\"6\">Подключён</TD></TR>\n";
+	//print "<TR><TD COLSPAN=\"6\">РџРѕРґРєР»СЋС‡С‘РЅ</TD></TR>\n";
 	$sql = "select mtt.t_type_name, sum(transaction_sum) as s_um, max(mt.transaction_date) as ltd, count(*) as cnt from m_transactions mt, m_transaction_types  mtt where mt.t_type_id=mtt.t_type_id and mtt.Type_sign<0 and transaction_date>='$bd' and  transaction_date<'$ed' ";
 	if($bg>0)	{
 		$sql .= " and mt.budget_id=$bg ";
@@ -133,7 +135,7 @@ if($conn)	{
 	$sd = 0;
 	$c_class = "dark";
 	if($res)	{
-            //print "<TR><TD COLSPAN=\"6\">Запрос пошёл</TD></TR>\n";
+            //print "<TR><TD COLSPAN=\"6\">Р—Р°РїСЂРѕСЃ РїРѕС€С‘Р»</TD></TR>\n";
             while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
                 print "<TR class=\"$c_class\">\n";
                 if(strcmp($c_class,"dark")==0)	{
@@ -158,15 +160,15 @@ if($conn)	{
             print "<TR><TD COLSPAN=\"6\">SQL=\"$sql\"<BR>$message</TD></TR>\n";
 	}
 	print "</TABLE>\n";
-	print_title("Потребительская активность с $bd по $ed");
+	print_title("РџРѕС‚СЂРµР±РёС‚РµР»СЊСЃРєР°СЏ Р°РєС‚РёРІРЅРѕСЃС‚СЊ СЃ $bd РїРѕ $ed");
 	print "<TABLE WIDTH=\"100%\" BORDER=\"1\">\n";
 	print "<TR>\n";
-	print "<TH>Кто</TH>\n";
-	print "<TH>Сумма</TH>\n";
-	print "<TH TITLE=\"относится к последней покупке\">Дата и время</TH>\n";
-	print "<TH>Количество</TH>\n";
+	print "<TH>РљС‚Рѕ</TH>\n";
+	print "<TH>РЎСѓРјРјР°</TH>\n";
+	print "<TH TITLE=\"РѕС‚РЅРѕСЃРёС‚СЃСЏ Рє РїРѕСЃР»РµРґРЅРµР№ РїРѕРєСѓРїРєРµ\">Р”Р°С‚Р° Рё РІСЂРµРјСЏ</TH>\n";
+	print "<TH>РљРѕР»РёС‡РµСЃС‚РІРѕ</TH>\n";
 	print "</TR>\n";
-	//print "<TR><TD COLSPAN=\"6\">Подключён</TD></TR>\n";
+	//print "<TR><TD COLSPAN=\"6\">РџРѕРґРєР»СЋС‡С‘РЅ</TD></TR>\n";
 	$sql = "select mu.user_name, sum(transaction_sum) as s_um, max(mt.transaction_date) as ltd, count(*) as cnt, mtt.Type_sign from m_transactions mt, m_users mu, m_transaction_types mtt where mt.user_id=mu.user_id and mt.t_type_id=mtt.t_type_id and transaction_date>='$bd' and transaction_date<'$ed' ";
 	if($bg>0)	{
 		$sql .= " and mt.budget_id=$bg ";
@@ -177,7 +179,7 @@ if($conn)	{
 	$sd = 0;
 	$c_class = "dark";
 	if($res)	{
-            //print "<TR><TD COLSPAN=\"6\">Запрос пошёл</TD></TR>\n";
+            //print "<TR><TD COLSPAN=\"6\">Р—Р°РїСЂРѕСЃ РїРѕС€С‘Р»</TD></TR>\n";
             while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
                 print "<TR class=\"$c_class\">\n";
                 if(strcmp($c_class,"dark")==0)	{
@@ -208,7 +210,7 @@ if($conn)	{
             print "<TR><TD COLSPAN=\"6\">SQL=\"$sql\"<BR>$message</TD></TR>\n";
 	}
 	print "</TABLE>\n";
-	print_buttons();
+	print_buttons($conn);
 	print "</form>\n";
 }
 
