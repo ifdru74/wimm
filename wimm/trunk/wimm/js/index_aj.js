b1abf7fbe13dbe13d7d1ace0d8248eb5bc7600a9
Alexey Shtykov <ifd2000@mail.ru>
2015-05-08 19:23:12 +0400

    UTF-8

--- wimm/trunk/wimm/js/index_aj.js
+++ wimm/trunk/wimm/js/index_aj.js
@@ -76,7 +76,7 @@ function onTxComplete(jqXHR, textStatus )
                 }
             }
             else
-                alert("Пустой ответ!");
+                alert("РџСѓСЃС‚РѕР№ РѕС‚РІРµС‚!");
         }
     }
     else {
@@ -104,7 +104,7 @@ function tx_submit()
                         var nn = new Number(v);
                         if(nn<=a_fields[i].val)   {
                             inv_idx = i;
-                            msg = "Недопустимое значение <=" + a_fields[i].val.toString();
+                            msg = "РќРµРґРѕРїСѓСЃС‚РёРјРѕРµ Р·РЅР°С‡РµРЅРёРµ <=" + a_fields[i].val.toString();
                         }
                         else {
                             reqStr += ("&" + a_fields[i].id + "=" + encodeURIComponent(v));
@@ -121,12 +121,12 @@ function tx_submit()
             }
             else    {
                 inv_idx = i;
-                msg = "Слишком короткое значение"
+                msg = "РЎР»РёС€РєРѕРј РєРѕСЂРѕС‚РєРѕРµ Р·РЅР°С‡РµРЅРёРµ"
             }
         }
         else    {
             inv_idx = i;
-            msg = "Значение не установлено"
+            msg = "Р—РЅР°С‡РµРЅРёРµ РЅРµ СѓСЃС‚Р°РЅРѕРІР»РµРЅРѕ"
         }
     }
     if(inv_idx>=0&&inv_idx<a_fields.length)  {
@@ -161,7 +161,7 @@ function sel_row(row_id)
         $("#ADD_BTN").show();
         $("#OK_BTN").hide();
         $("#DEL_BTN").hide();
-        $("#dlg_box_cap").text("Добавление записи");
+        $("#dlg_box_cap").text("Р”РѕР±Р°РІР»РµРЅРёРµ Р·Р°РїРёСЃРё");
         s1 = "";
         for(i=0; i<a_fields.length; i++)    {
             if(a_fields[i].id.indexOf("t_user")==0)
@@ -176,7 +176,7 @@ function sel_row(row_id)
         $("#ADD_BTN").hide();
         $("#OK_BTN").show();
         $("#DEL_BTN").show();
-        $("#dlg_box_cap").text("Изменение записи");
+        $("#dlg_box_cap").text("РР·РјРµРЅРµРЅРёРµ Р·Р°РїРёСЃРё");
         s1 = "";
         for(i=0; i<a_fields.length; i++)    {
             s1 = "";
